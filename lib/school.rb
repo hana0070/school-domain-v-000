45cab63bdc363bd6f50f93f124ff4709a1dee909
HANA LEANDER <hana0070@gmail.com>
2019-05-01 14:13:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,6 +11,8 @@ class School
   def add_student(student_name, grade)
     roster[grade] ||= []
     roaster[grade] << student_name
+    #It is an assignment operator which means: or assign this value to a variable.
+    #So if you did something like x ||= ythis meansx || x = y so if x is nil or false set x to be the value of y.
   end
 
   def grade(num)
